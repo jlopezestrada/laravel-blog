c1464945924105356090cc487cf9ac30a8d75898
José Ángel López <jose.a.l.e@hotmail.com>
2024-05-15 18:53:25 +0200

    feature: add routes and empty views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
+Route::resource('posts', PostController::class);
+Route::post('comments', [CommentController::class, 'store'])->name('comments.store');
 Route::get('/', function () {
     return view('welcome');
 });
