fb8cbe83f0070105465116bf5445a6b0b5ad69d3
José Ángel López <jose.a.l.e@hotmail.com>
2024-05-19 22:23:40 +0200

    feature: add initial migrations

--- database/migrations/2024_05_19_201609_create_posts_table.php
+++ database/migrations/2024_05_19_201609_create_posts_table.php
@@ -4,9 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
@@ -16,7 +19,10 @@ class CreatePostsTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('posts');
     }
--- database/migrations/2024_05_19_201631_create_comments_table.php
+++ database/migrations/2024_05_19_201631_create_comments_table.php
@@ -4,21 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->text('content');
-            $table->unsignedBigInteger('post_id');
+            $table->foreignId('post_id')->contrained()->onDelete('cascade');
+            $table->string('author');
+            $table->text('comment');
             $table->timestamps();
-
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('comments');
     }
