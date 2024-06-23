a90e45f2f7cc6ef3a27d0e1f89f0a2d27fc03457
José Ángel López <jose.a.l.e@hotmail.com>
2024-06-23 22:00:37 +0200

    feature: add routes and controller

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\CommentController;
 
 Route::resource('posts', PostController::class);
 Route::post('comments', [CommentController::class, 'store'])->name('comments.store');
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/admin/posts/create', [PostController::class, 'create']);
+Route::post('/admin/posts', [PostController::class, 'store']);
+Route::get('/admin/posts/{id}/edit', [PostController::class, 'edit']);
+Route::put('/admin/posts/{id}', [PostController::class, 'update']);
+Route::delete('/admin/posts/{id}', [PostController::class, 'destroy']);
